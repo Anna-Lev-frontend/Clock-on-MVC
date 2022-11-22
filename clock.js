e3035099dcd523feb224b215497ca6e34861be12
Anna Levkovich <7604442@gmail.com>
2022-11-22 23:52:11 +0300

    написала model

---
+++ clock.js
@@ -0,0 +1,20 @@
+class Clock {
+    constructor(carrentUTC) {
+        this.carrentUTC = carrentUTC;
+    }
+    start() {
+        if (!this.timerId) {
+            this.timerId = setInterval(() => {
+                const date = new Date();
+                this.hour = date.getUTCHours() + this.carrentUTC;
+                this.min = date.getUTCMinutes();
+                this.sec = date.getUTCSeconds();
+            }, 1000);
+        }
+    }
+    stop() {
+        if (this.timerId) {
+            clearInterval(this.timerId)
+        }
+    }
+}
---
+++ index.js
@@ -0,0 +1,86 @@
+const clockModel = 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const makePointsDeal = () => {
+//     const points = new Array(12).fill(null);
+//     const arrayDiv = points.map((value, index) => {
+//         const div = document.createElement('div');
+      
+//        div.style = `transform: translate(0px, -200px) rotate(${(index + 1) * 30}deg); transform-origin: center 235px;`
+
+//         div.classList.add('point');
+//         div.innerText = index + 1;
+//         return div;
+//     })
+//     return arrayDiv
+// }
+
+// const makeArrows = () => {
+//     const makeArrows = document.createElement('div');
+//     makeArrows.classList.add('make-arrows')
+//     const arrowHour = document.createElement('div');
+//     arrowHour.classList.add('hr');
+        
+//     const arrowMin = document.createElement('div');
+//     arrowMin.classList.add('mn');
+  
+//     const arrowSec = document.createElement('div');
+//     arrowSec.classList.add('sc');
+//     makeArrows.append(arrowHour,arrowMin,arrowSec);
+
+//     return makeArrows;
+// }
+
+// const makeDial = () => {
+//     const mainDial = document.createElement('div');
+//     mainDial.classList.add('main-dial');
+//     const pointsDeal = makePointsDeal();
+//     const arrow = makeArrows();
+//     mainDial.append(...pointsDeal, arrow)
+
+//     return mainDial;
+// }
+// const clock = setInterval((
+// ) => {
+//     const deg = 6;
+//     const date = new Date();
+//     const min = date.getMinutes() * deg;
+//     const sec = date.getSeconds() * deg;
+//     const hour = date.getHours() * 30;
+//     const hr = document.querySelector('.hr');
+//     const sc = document.querySelector('.sc');
+//     const mn = document.querySelector('.mn')
+//     hr.style.transform = `rotateZ(${(hour) + (min / 12)}deg)`;
+//     mn.style.transform = `rotateZ(${min}deg)`;
+//      sc.style.transform = `rotateZ(${sec}deg)`;
+
+// },);
+
+// document.body.append(makeDial())
