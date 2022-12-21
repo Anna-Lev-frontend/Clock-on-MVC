55f5c063cf453c3ad02760bbfc40924ff70bad2b
Anna Levkovich <7604442@gmail.com>
2022-12-21 14:21:42 +0300

    fix: прописала циферблат

--- ClockViewCanvas.js
+++ ClockViewCanvas.js
@@ -24,14 +24,14 @@ export default class ClockViewCanvas {
         this.canvas.addEventListener('click', (event) => {
             const x = event.offsetX;
             const y = event.offsetY;
-            
+
             this.controller.startClickHandler(this.generateTemplate.bind(this))
         })
 
 
     }
     generateTemplate() {
-        this.ctx.clearRect(0,0, this.canvas.width,this.canvas.height)
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
         //this.ctx.fillStyle = 'white';
         //this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
@@ -44,22 +44,22 @@ export default class ClockViewCanvas {
 
         this.ctx.beginPath();
         this.ctx.arc(100, 120, 100, 0, 2 * Math.PI, false);
-        this.ctx.fillStyle = 'red';
+        this.ctx.fillStyle = 'white';
         this.ctx.fill();
         this.ctx.lineWidth = 1;
-        this.ctx.strokeStyle = 'red';
+        this.ctx.strokeStyle = 'white';
         this.ctx.stroke();
-        [{ x: 100, y: 40 }, { x: 135, y: 50 }, { x: 165, y: 75 }, { x: 175, y: 110 }, { x: 170, y: 150 }, { x: 145, y: 185 }, { x: 105, y: 200 }, { x: 70, y: 190 }, { x: 35, y: 170 }, { x: 20, y: 140 }, { x: 20, y: 110 }, { x: 20, y: 90 }].forEach((item, index) => {
+        [{ x: 145, y: 50 }, { x: 165, y: 80 }, { x: 175, y: 115 }, { x: 170, y: 155 }, { x: 145, y: 185 }, { x: 105, y: 195 }, { x: 60, y: 185 }, { x: 35, y: 155 }, { x: 25, y: 115 }, { x: 35, y: 75 }, { x: 60, y: 50 }, { x: 100, y: 40 }].forEach((item, index) => {
             this.ctx.beginPath();
             this.ctx.arc(item.x, item.y, 11, 0, 2 * Math.PI, false);
-            this.ctx.fillStyle = 'green';
+            this.ctx.fillStyle = 'red';
             this.ctx.fill();
             this.ctx.lineWidth = 1;
-            this.ctx.strokeStyle = 'green';
+            this.ctx.strokeStyle = 'red';
             this.ctx.stroke();
             this.ctx.font = '14px sans-serif';
             this.ctx.fillStyle = 'black';
-            this.ctx.fillText(index + 1, item.x, item.y);
+            this.ctx.fillText(index + 1, (item.x) - 5, item.y +3);
 
         })
 
@@ -67,9 +67,9 @@ export default class ClockViewCanvas {
     createArrows() {
 
         const arrows = [
-            { color: 'black', height: -50, width: 5, deg: this.controller.hour * 30 },
+            { color: 'black', height: -50, width: 7, deg: this.controller.hour * 30 },
             { color: 'black', height: -60, width: 6, deg: this.controller.min * 6 },
-            { color: 'yellow', height: -70, width: 4, deg: this.controller.sec * 6 }]
+            { color: 'red', height: -70, width: 4, deg: this.controller.sec * 6 }]
         this.ctx.translate(100, 120);
         arrows.forEach((item) => {
             this.ctx.strokeStyle = 'black';
